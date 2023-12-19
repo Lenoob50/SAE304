a172ef3acc6e79fe03587f58730774ee7f5f2baa
clemence6565217 <clemence.charles@outlook.be>
2023-12-19 11:30:17 +0100

    Remove .gitignore

--- DHCP/dhcp.py
+++ DHCP/dhcp.py
@@ -4,12 +4,17 @@
 from scapy.all import *
 #Construction d'un paquet DHCP_DISCOVER à envoyer
 #Construction de la couche L1 Ethernet avec une adresse MAC de destination de multidiffusion et une adresse MAC source aléatoire avec la fonction "RandMAC" afin d'empêcher le serveur DHCP de déterminer l'expéditeur
-frame_discover = Ether(dst="ff:ff:ff:ff:ff:ff", src=RandMAC(), type=0x0800) \
+frame_discover_l1 = Ether(dst="ff:ff:ff:ff:ff:ff", src=RandMAC(), type=0x0800)
 #Construction de la couche L2 IP avec une adresse source par défaut et une adresse destination 255.255.255.255
-                / IP(src="0.0.0.0", dst="255.255.255.255") \
-#Construction de la couche L3 UDP avec
-                / UDP(dport=67,sport=68) \
-                / BOOTP(op=1, chaddr=RandMAC()) \
-                / DHCP(options=[("message-type","discover"), ("end")]) \
-
-sendp(DHCP_DISCOVER, iface="eth0",loop=1,verbose=1 )
+frame_discover_l2 = IP(src="0.0.0.0", dst="255.255.255.255")
+#Construction de la couche L3 UDP en spécifiant à quel port nous voulons que ce paquet soit envoyé, le port de destination est 67 et celui source est 68
+frame_discover_l3 = UDP(dport=67,sport=68)
+#Ajout de l'en-tête "BOOTP" afin de spécifier des options, "op=1" indique qu'il s'agit d'une demande de démarrage, de plus, l'adresse MAC du client se voit attribuer une adresse aléatoire
+frame_discover_l4 = BOOTP(op=1, chaddr=RandMAC())
+#Demande d'adresse IP au serveur DHCP en spécifiant qu'il s'agit d'un message de découverte
+frame_discover_l5 = DHCP(options=[("message-type","discover"), ("end")])
+#Encapsulation des différentes couches afin d'obtenir un paquet
+frame_discover = frame_discover_l1 / frame_discover_l2 / frame_discover_l3 / frame_discover_l4 / frame_discover_l5
+#Envoie du segment grâce à la fonction "sendp", le DHCP fonctionnant sur la couche L2 du modèle OSI
+#Envoie du paquet à l'intérieur d'une boucle "loop=1" afin d'effectuer plusieurs interrogations demandant les adresses IP
+sendp(frame_discover, iface="eth0",loop=1,verbose=1 )
