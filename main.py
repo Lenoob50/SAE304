3a7f0af4aca43303fdf3a05bdd34c714b8974030
Lenoob50 <gthe25597@gmail.com>
2024-01-17 18:31:47 +0100

    Add GUI support and args

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from STP import *
+import argparse
+
+def launch(args):
+    if(args!=None):
+        print('Ceci est le test de non mode graphique')
+    else:
+        print('Ceci est le test de mode graphique')
+def arguments():
+    parser = argparse.ArgumentParser(description="SAE 304 - Découvrir le pentesting")
+    parser.add_argument('--no-gui','-n',type=str,required=False,help="Lancement en mode graphique ou non")
+    args = parser.parse_args()
+    launch(args.no_gui)
+
+arguments()
+
+
+
