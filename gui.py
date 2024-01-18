ef404728283a14e2c1785e56644c7a636dd5587a
Lenoob50 <gthe25597@gmail.com>
2024-01-18 11:56:08 +0100

    Fenetre DHCP en cours de creation

--- gui.py
+++ gui.py
@@ -1,28 +1,44 @@
 from tkinter import *
 from tkinter import messagebox
 from scapy.all import *
+from DHCP.dhcp import dhcp
+
 
 def nyi():
     msg = messagebox.showinfo("Erreur dans la matrice", "Pas ecnore implémenté\n Revenez plus tard ;)")
 
+
+
 def dhcp_window():
     dhcp_win = Tk()
     dhcp_win.title("RT2@hacker - DHCP")
-    dhcp_win.geometry("400x250")
+    dhcp_win.geometry("600x300")
     dhcp_win.resizable(False,False)
     label_dhcp = Label(dhcp_win,text="Attaque DHCP Starvation")
-    label_dhcp.place(x=125,y=10)
+    label_dhcp.place(x=225,y=10)
     interfaces = get_if_list()
-    inet_list = Listbox(dhcp_win)
-    print(interfaces)
+    inet_list = Listbox(dhcp_win,height=3)
+    for i in range(len(interfaces)):
+        inet_list.insert(i+1,str(interfaces[i]))
+    inet_list.place(x=20,y=30)
+    def selected_int():
+        int_use = ""
+        for i in inet_list.curselection():
+            int_use=inet_list.get(i)
+        return int_use
+    def launch():
+        dhcp(selected_int())
+    button = Button(dhcp_win, text="Lancer l'attaque", width=15, command=launch)
+    button.place(x=20, y=200)
     dhcp_win.mainloop()
 
-dhcp_window()
 
+
+dhcp_window()
 def gui():
     main = Tk()
     main.title("RT2@hacker")
-    main.geometry("400x250")
+    main.geometry("800x500")
     main.resizable(False, False)
     bienvenue = Label(main, text="Bienvenue, quelle attaque voulez-vous utiliser ?")
     bienvenue.place(x=80, y=10)
